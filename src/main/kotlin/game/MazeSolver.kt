b13125f51c9649922f0aa94a450f78c8d11780fa
Gresanu Emanuel Vasile <gresanu.emanuel.vasile@gmail.com>
2019-08-11 14:12:44 +0300

    Create code body for multiple roads search.

--- src/main/kotlin/game/MazeSolver.kt
+++ src/main/kotlin/game/MazeSolver.kt
@@ -10,6 +10,9 @@ import constants.Constants.playerEndGameAtCoordinateYRoud1
 import constants.Constants.playerEndGameAtCoordinateYRoud2
 import constants.Constants.playerEndGameAtCoordinateYSingleRound
 import constants.Constants.playerMoveDirections
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 typealias SquareMaze = List<List<Int>>
 
@@ -19,22 +22,23 @@ class MazeSolver(
     private val playerStartPositionY: Int = 0
 ) {
 
-    private val listOfMoves = mutableListOf<VisitedCoordinates>()
-    private lateinit var currentPosition: VisitedCoordinates
-
     fun playGameForOnlyOneRoad(): List<VisitedCoordinates> {
         //Add the player start position to the list of moves
+        val listOfMoves = mutableListOf<VisitedCoordinates>()
         listOfMoves.addPlayerStartPositionCoordinates(playerStartPositionX, playerStartPositionY)
         //Set current position
-        currentPosition = listOfMoves[0]
+        var currentPosition = listOfMoves[0]
         var hadFoundTheExit = false
         while (!hadFoundTheExit) {
             //I can't use foreach because I lose continue keyword
             //For current position I will check all directions to see if I have a valid move
             for (direction in playerMoveDirections) {
+                //Here I don't care about the last argument that is the name of the thread. I will take this argument into
+                //consideration only in function playGameForMultipleRoadsAndReturnTheShortestOne
                 val nextMove = VisitedCoordinates(
                     currentPosition.mazeXPosition + direction[ROW],
-                    currentPosition.mazeYPosition + direction[COLUMN]
+                    currentPosition.mazeYPosition + direction[COLUMN],
+                    Thread.currentThread().name
                 )
 
                 //Before continuing with any other validation. I need to check if the next position is not out of maze bounds
@@ -63,17 +67,41 @@ class MazeSolver(
             }
         }
 
-
         return listOfMoves
     }
 
     fun playGameForMultipleRoadsAndReturnTheShortestOne(): List<VisitedCoordinates> {
         //I am starting this game from the premise that every step the player makes has the same value
         //So at the end I will count the total number of steps that I had to make in order to reach the end
+        val howManyTestToDo = 90
+        // I will run every test on one thread. Also I will create a threads pool that has half threads of the tests
+        // that I will run. "howManyTestToDo shr 1" is equel with "howManyTestToDo / 2"
+        val executor = Executors.newFixedThreadPool((howManyTestToDo shr 1))
+        //Create list to store all the threads
+        val listOfFutureThreads = mutableListOf<Future<List<VisitedCoordinates>>>()
+        for(currentTest in 0 until howManyTestToDo) {
+            listOfFutureThreads.add(executor.submit<List<VisitedCoordinates>> { playGameForOnlyOneRoad() })
+        }
 
+        // Now because I can't cancel the JVM thread I will block it until all future tasks are completed.
+        // This is not code for production and I know that this is very bad. So only this time I will break the rules
 
+        while (listOfFutureThreads.all { it.isDone }) {
+            TimeUnit.SECONDS.sleep(1L)
+        }
 
-        return emptyList()
+        //Now I now, that all future tasks had finished. Now I need to store the best restuls
+        var bestTime = Int.MAX_VALUE
+        var bestPath = emptyList<VisitedCoordinates>()
+        listOfFutureThreads.forEach { futureTask ->
+            val pathToExit = futureTask.get()
+            if(bestTime < pathToExit.size) {
+                bestTime = pathToExit.size
+                bestPath = pathToExit
+            }
+        }
+
+        return bestPath
     }
 
 }
@@ -98,4 +126,6 @@ fun MutableList<VisitedCoordinates>.isExplored(nextMove: VisitedCoordinates) =
     any { it.mazeXPosition == nextMove.mazeXPosition && it.mazeYPosition == nextMove.mazeYPosition }
 
 fun MutableList<VisitedCoordinates>.addPlayerStartPositionCoordinates(row: Int, column: Int) =
-    add(VisitedCoordinates(row, column))
+    add(VisitedCoordinates(row, column, Thread.currentThread().name))
+
+fun MutableList<VisitedCoordinates>.howManyStepsHadITookFromStartToStop() = this.size
